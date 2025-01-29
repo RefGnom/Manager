462783c6b28aa2f4bd39a22d53058aec6213c537
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-29 21:40:26 +0500

    folder rename

---
+++ Manager.Tool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Manager.Tool;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var argumentsString = string.Join('\n', args);
+        Console.WriteLine($"Я выполнился! Параметры: {argumentsString}");
+    }
+}
--- Manager/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Manager.Tool;
-
-public class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
