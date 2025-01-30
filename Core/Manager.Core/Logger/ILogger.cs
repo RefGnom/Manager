8d92b4a9ff781c26e44d1d024f2fced22ab42ae5
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-30 21:48:49 +0500

    Атрибуты на текст у логера

--- Core/Manager.Core/Logger/ILogger.cs
+++ Core/Manager.Core/Logger/ILogger.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Manager.Core.Logger;
 
 public interface ILogger<TContext>
 {
-    public void LogInfo(string text, params object?[] args);
-    public void LogWarn(string text, params object?[] args);
-    public void LogError(string text, params object?[] args);
+    public void LogInfo([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string text, params object?[] args);
+    public void LogWarn([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string text, params object?[] args);
+    public void LogError([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string text, params object?[] args);
 }
