a62c2e48d6bcde9698d996954526c628b0b4d35b
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-31 19:57:27 +0500

    когда нибудь я перестану забывать this для экстеншенов

--- Core/Manager.Core/LinqExtensions/ForeachLinqExtensions.cs
+++ Core/Manager.Core/LinqExtensions/ForeachLinqExtensions.cs
@@ -2,7 +2,7 @@ namespace Manager.Core.LinqExtensions;
 
 public static class ForeachLinqExtensions
 {
-    public static void Foreach<T>(IEnumerable<T> source, Action<T> action)
+    public static void Foreach<T>(this IEnumerable<T> source, Action<T> action)
     {
         foreach (var item in source)
         {
