016140cc09c2734c0570957e70fdf995e7067554
kk7k3r <d.ostrozhnoy@gmail.com>
2025-02-02 19:05:24 +0500

    DbContext

---
+++ Manager.Data/ManagerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class ManagerDbContext : DbContext
+{
+    public ManagerDbContext(DbContextOptions<ManagerDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TimerEntity> Timers { get; set; }
+    public DbSet<TimerSessionEntity> TimerSessions { get; set; }
+}
