2b6b7fbcc31ce323a73aeacfb7a43986b0db9d19
kk7k3r <d.ostrozhnoy@gmail.com>
2025-02-04 17:43:42 +0500

    Repository

---
+++ Manager.Data/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Repositories;
+
+public interface IRepository<T>
+    where T : class
+{
+    Task<List<T>> GetAllAsync();
+    Task<T> TryGetByIdAsync(Guid id);
+    Task CreateAsync(T item);
+    Task UpdateAsync(Guid id, T item);
+    Task DeleteAsync(Guid id);
+    Task SaveAsync();
+}
