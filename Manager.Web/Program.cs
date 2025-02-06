51b4bfa672460f91ea953ea6cd2ca810bf8be1b0
kk7k3r <d.ostrozhnoy@gmail.com>
2025-02-06 21:23:30 +0500

    test controller

--- Manager.Web/Program.cs
+++ Manager.Web/Program.cs
@@ -4,14 +4,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ManagerDbContext>(
-    options =>
-    {
-        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-    }
-    );
+    options => { options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")); }
+);
 
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
