530b99292b985e42974da50dbdeb73e11700faa0
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-02-09 19:49:17 +0500

    Ещё апи методы и статусы для таймеров

--- Service/ManagerService.Client/ServiceModels/UserTimersRequest.cs
+++ Service/ManagerService.Client/ServiceModels/UserTimersRequest.cs
@@ -3,4 +3,6 @@ namespace ManagerService.Client.ServiceModels;
 public class UserTimersRequest
 {
     public required User User { get; set; }
+    public required bool WithArchived { get; set; }
+    public required bool WithDeleted { get; set; }
 }
