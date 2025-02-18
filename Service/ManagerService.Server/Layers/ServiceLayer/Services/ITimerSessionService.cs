dd069507fab702d73ddd85a44bd4811017bca471
dostr <d.ostrozhnoy@gmail.com>
2025-02-18 21:49:22 +0500

    Session service contract

---
+++ Service/ManagerService.Server/Layers/ServiceLayer/Services/ITimerSessionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ManagerService.Server.ServiceModels;
+
+namespace ManagerService.Server.Layers.ServiceLayer.Services;
+
+public interface ITimerSessionService
+{
+    public Task StartSessionAsync(Guid timerId, DateTime startTime);
+    public Task<List<TimerSessionDto>> SelectByTimerAsync(Guid timerId);
+    public Task StopTimerSessionAsync(Guid id);
+}
--- Service/ManagerService.Server/ServiceModels/TimerDto.cs
+++ Service/ManagerService.Server/ServiceModels/TimerDto.cs
@@ -5,11 +5,11 @@ namespace ManagerService.Server.ServiceModels;
 
 public class TimerDto
 {
-    public required Guid Id { get; set; }
+    public required Guid Id { get; init; }
     public required Guid UserId { get; init; }
     public required string Name { get; init; }
-    public required DateTime StartTime { get; set; }
+    public required DateTime? StartTime { get; set; }
     public required TimeSpan? PingTimeout { get; set; }
     public required TimerSessionDto[] Sessions { get; set; }
-    public required TimerStatus Status { get; init; }
+    public required TimerStatus Status { get; set; }
 }
