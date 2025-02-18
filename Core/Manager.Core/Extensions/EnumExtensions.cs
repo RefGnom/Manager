748da68d932c212a4c2b1969ca8a372fa4edbc42
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-02-19 00:31:10 +0500

    Оказывается, такой экстеншен уже есть)

--- Core/Manager.Core/Extensions/EnumExtensions.cs
+++ Core/Manager.Core/Extensions/EnumExtensions.cs
@@ -1,23 +1,14 @@
 using System;
 using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
+using Microsoft.OpenApi.Extensions;
 
 namespace Manager.Core.Extensions;
 
 public static class EnumExtensions
 {
-    public static string GetDescription(Enum value)
+    public static string GetDescription(this Enum value)
     {
-        var descriptionAttribute = value.GetAttribute<DescriptionAttribute>();
+        var descriptionAttribute = value.GetAttributeOfType<DescriptionAttribute>();
         return descriptionAttribute?.Description ?? "";
-    }
-
-    public static T? GetAttribute<T>(this Enum value)
-        where T : Attribute
-    {
-        var type = value.GetType();
-        var memInfo = type.GetMember(value.ToString());
-        return memInfo.Single().GetCustomAttribute<T>();
     }
 }
