f00aac8d33788dcd705afe24f9f28dccaf177792
dostr <d.ostrozhnoy@gmail.com>
2025-02-21 23:24:05 +0500

    Migration: UserId & Namer is unique index

--- Service/ManagerService.Server/Program.cs
+++ Service/ManagerService.Server/Program.cs
@@ -1,7 +1,9 @@
+using Manager.Core.DateTimeProvider;
 using ManagerService.Server.Configurators;
 using ManagerService.Server.Layers.Api.Converters;
 using ManagerService.Server.Layers.DbLayer;
 using ManagerService.Server.Layers.RepositoryLayer;
+using ManagerService.Server.Layers.ServiceLayer.Factories;
 using ManagerService.Server.Layers.ServiceLayer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
@@ -16,8 +18,13 @@ builder.Services.AddMapper();
 builder.Services.AddScoped<ITimerRepository, TimerRepository>();
 builder.Services.AddScoped<ITimerSessionRepository, TimerSessionRepository>();
 
-builder.Services.AddTransient<ITimerService, TimerService>();
-builder.Services.AddSingleton<ITimerHttpModelsConverter, TimerHttpModelsConverter>();
+builder.Services.AddScoped<IDateTimeProvider, DateTimeProvider>();
+builder.Services.AddScoped<ITimerSessionService, TimerSessionService>();
+builder.Services.AddScoped<ITimerService, TimerService>();
+builder.Services.AddScoped<ITimerDtoFactory, TimerDtoFactory>();
+builder.Services.AddScoped<ITimerSessionHttpModelConverter, TimerSessionHttpModelConverter>();
+
+builder.Services.AddScoped<ITimerHttpModelsConverter, TimerHttpModelsConverter>();
 
 builder.Services.AddScoped<ManagerDbContext>();
 
