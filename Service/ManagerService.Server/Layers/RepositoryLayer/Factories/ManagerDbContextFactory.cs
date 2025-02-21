5e9eee30a4a36488b918b6f5340ef915ffdda14e
dostr <d.ostrozhnoy@gmail.com>
2025-02-22 03:27:23 +0500

    DbContextFactory

---
+++ Service/ManagerService.Server/Layers/RepositoryLayer/Factories/ManagerDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using ManagerService.Server.Layers.DbLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagerService.Server.Layers.RepositoryLayer.Factories;
+
+public class ManagerDbContextFactory(DbContextOptions<ManagerDbContext> options)
+    : IDbContextFactory<ManagerDbContext>
+{
+    private readonly DbContextOptions<ManagerDbContext> _options = options;
+
+    public ManagerDbContext CreateDbContext()
+    {
+        return new ManagerDbContext(_options);
+    }
+}
