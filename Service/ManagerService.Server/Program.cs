368b924319caa28fd2a644c26989fba22f2a4c42
dostr <d.ostrozhnoy@gmail.com>
2025-02-22 15:10:20 +0500

    Autoregistration without oshibka

--- Service/ManagerService.Server/Program.cs
+++ Service/ManagerService.Server/Program.cs
@@ -1,14 +1,8 @@
-// using Manager.Core.DateTimeProvider;
+using Manager.Core.DateTimeProvider;
 using Manager.Core.DependencyInjection;
 using ManagerService.Server.Configurators;
 using ManagerService.Server.Layers.DbLayer;
-// using ManagerService.Server.Layers.Api.Converters;
-// using ManagerService.Server.Layers.DbLayer;
-// using ManagerService.Server.Layers.RepositoryLayer;
-// using ManagerService.Server.Layers.RepositoryLayer.Factories;
-// using ManagerService.Server.Layers.RepositoryLayer.Repositories;
-// using ManagerService.Server.Layers.ServiceLayer.Factories;
-// using ManagerService.Server.Layers.ServiceLayer.Services;
+using ManagerService.Server.Layers.RepositoryLayer.Factories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -19,27 +13,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMapper();
 
-// builder.Services.AddSingleton<ITimerRepository, TimerRepository>();
-// builder.Services.AddSingleton<ITimerSessionRepository, TimerSessionRepository>();
-//
-// builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
-// builder.Services.AddSingleton<ITimerSessionService, TimerSessionService>();
-// builder.Services.AddSingleton<ITimerService, TimerService>();
-// builder.Services.AddSingleton<ITimerDtoFactory, TimerDtoFactory>();
-// builder.Services.AddSingleton<ITimerSessionHttpModelConverter, TimerSessionHttpModelConverter>();
-
-
 builder.Services.AddSingleton(s =>
     new DbContextOptionsBuilder<ManagerDbContext>()
         .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")).Options
 );
+builder.Services.AddSingleton<IDbContextFactory<ManagerDbContext>, ManagerDbContextFactory>();
+builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
-builder.Services.AddSingleton<ManagerDbContext>();
-
-// builder.Services.AddSingleton<IDbContextFactory<ManagerDbContext>, ManagerDbContextFactory>();
-// builder.Services.AddSingleton<ITimerHttpModelsConverter, TimerHttpModelsConverter>();
-
-AutoRegistrationExtensions.UseAutoRegistrationForCurrentAssembly(builder.Services);
+builder.Services.UseAutoRegistrationForCurrentAssembly();
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
