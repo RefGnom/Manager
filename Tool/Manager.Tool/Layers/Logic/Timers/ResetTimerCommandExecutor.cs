7a8f85041287fbe8e3e751bc07132dfb45f818b6
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-03-02 04:15:09 +0500

    reset timer impl

--- Tool/Manager.Tool/Layers/Logic/Timers/ResetTimerCommandExecutor.cs
+++ Tool/Manager.Tool/Layers/Logic/Timers/ResetTimerCommandExecutor.cs
@@ -1,14 +1,41 @@
-using System;
+using System.Net;
 using System.Threading.Tasks;
 using Manager.Tool.Layers.Logic.CommandsCore;
+using Manager.Tool.Layers.Presentation;
+using ManagerService.Client;
 
 namespace Manager.Tool.Layers.Logic.Timers;
 
 public class ResetTimerCommandExecutor(
-    IToolCommandFactory toolCommandFactory) : CommandExecutorBase<ResetTimerCommand>(toolCommandFactory)
+    IToolCommandFactory toolCommandFactory,
+    ITimerRequestFactory timerRequestFactory,
+    ITimerServiceApiClient timerServiceApiClient,
+    IToolWriter toolWriter
+) : CommandExecutorBase<ResetTimerCommand>(toolCommandFactory)
 {
-    protected override Task ExecuteAsync(CommandContext context, ResetTimerCommand command)
+    private readonly ITimerRequestFactory _timerRequestFactory = timerRequestFactory;
+    private readonly ITimerServiceApiClient _timerServiceApiClient = timerServiceApiClient;
+    private readonly IToolWriter _toolWriter = toolWriter;
+
+    protected async override Task ExecuteAsync(CommandContext context, ResetTimerCommand command)
     {
-        throw new NotImplementedException();
+        var timerName = context.GetCommandArgument(command.CommandName) ?? TimerCommandConstants.DefaultTimerName;
+
+        var deleteTimerRequest = _timerRequestFactory.CreateDeleteTimerRequest(context.EnsureUser().Id, timerName);
+        var httpResponse = await _timerServiceApiClient.DeleteTimerAsync(deleteTimerRequest);
+
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            _toolWriter.WriteMessage("Таймер успешно сброшен");
+            return;
+        }
+
+        if (httpResponse.StatusCode is HttpStatusCode.NotFound)
+        {
+            _toolWriter.WriteMessage("Не нашли таймер с именем {0}", timerName);
+            return;
+        }
+
+        _toolWriter.WriteMessage("Ошибка при сбросе таймера: {0}", httpResponse.ResponseMessage);
     }
 }
