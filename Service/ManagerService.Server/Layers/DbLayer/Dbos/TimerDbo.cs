6adeba6d129c13522cb5520419bedfdfd99f2446
dostr <d.ostrozhnoy@gmail.com>
2025-03-02 11:08:33 +0500

    Session service

--- Service/ManagerService.Server/Layers/DbLayer/Dbos/TimerDbo.cs
+++ Service/ManagerService.Server/Layers/DbLayer/Dbos/TimerDbo.cs
@@ -5,7 +5,7 @@ namespace ManagerService.Server.Layers.DbLayer.Dbos;
 
 public class TimerDbo
 {
-    public required Guid Id { get; set; }
+    public required Guid Id { get; init; }
     public required Guid UserId { get; init; }
     public required string Name { get; init; }
     public required DateTime StartTime { get; init; }
