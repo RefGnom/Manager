d54d3182b32d54c1e09cb28d1f978bb57000fd87
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-03-08 14:51:15 +0500

    Добавил команду help. Пока что выводит список всех пространств и команд, принадлежащим им

--- Tool/Manager.Tool/Layers/Logic/CommandsCore/CommandSpace.cs
+++ Tool/Manager.Tool/Layers/Logic/CommandsCore/CommandSpace.cs
@@ -1,3 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Manager.Core.Extensions.LinqExtensions;
+
 namespace Manager.Tool.Layers.Logic.CommandsCore;
 
-public record CommandSpace(string Description, params string[] Values);
+public record CommandSpace(
+    string Description,
+    params string[] Values
+);
+
+public class CommandSpaceEqualityComparer : IEqualityComparer<CommandSpace?>
+{
+    public bool Equals(CommandSpace? first, CommandSpace? second)
+    {
+        if (ReferenceEquals(first, second))
+        {
+            return true;
+        }
+
+        if (ReferenceEquals(first, null))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(second, null))
+        {
+            return false;
+        }
+
+        if (first.GetType() != second.GetType())
+        {
+            return false;
+        }
+
+        return first.Description == second.Description && first.Values.SequenceEqual(second.Values);
+    }
+
+    public int GetHashCode(CommandSpace obj)
+    {
+        return HashCode.Combine(obj.Description, obj.Values.JoinToString(' '));
+    }
+}
