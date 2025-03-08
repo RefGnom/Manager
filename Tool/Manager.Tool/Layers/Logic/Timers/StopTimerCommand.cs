7dc312e70f09bdfa5db0cd73e29bd49be14edf9c
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-03-08 12:56:52 +0500

    Переделал работу с командным пространством

--- Tool/Manager.Tool/Layers/Logic/Timers/StopTimerCommand.cs
+++ Tool/Manager.Tool/Layers/Logic/Timers/StopTimerCommand.cs
@@ -6,5 +6,6 @@ public class StopTimerCommand : IToolCommand
 {
     public string CommandName => "stop";
     public CommandSpace CommandSpace => TimerCommandConstants.TimersCommandSpace;
+    public CommandOptionInfo[] CommandOptions => [StopTimeOption];
     public CommandOptionInfo StopTimeOption { get; } = new("-t", "--stop_time", "Time when need stop timer");
 }
