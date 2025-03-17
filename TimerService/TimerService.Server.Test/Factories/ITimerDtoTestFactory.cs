6de3b94e4f6be2f80110414eabe348a36874ef28
dostr <d.ostrozhnoy@gmail.com>
2025-03-18 03:37:24 +0500

    CalculateElapsedTimes tests

---
+++ TimerService/TimerService.Server.Test/Factories/ITimerDtoTestFactory.cs
@@ -0,0 +1,8 @@
+﻿using Manager.TimerService.Server.ServiceModels;
+
+namespace TimerService.Server.Test.Factories;
+
+public interface ITimerDtoTestFactory
+{
+    public TimerDto CreateFromSessions(TimerSessionDto[] sessions);
+}
---
+++ TimerService/TimerService.Server.Test/Factories/TimerDtoTestFactory.cs
@@ -0,0 +1,21 @@
+﻿using Manager.TimerService.Client.ServiceModels;
+using Manager.TimerService.Server.ServiceModels;
+
+namespace TimerService.Server.Test.Factories;
+
+public class TimerDtoTestFactory: ITimerDtoTestFactory
+{
+    public TimerDto CreateFromSessions(TimerSessionDto[] sessions)
+    {
+        return new TimerDto()
+        {
+            Id = Guid.Empty,
+            UserId = Guid.Empty,
+            Name = string.Empty,
+            Sessions = sessions,
+            StartTime = DateTime.MinValue,
+            PingTimeout = null,
+            Status = TimerStatus.Created
+        };
+    }
+}
