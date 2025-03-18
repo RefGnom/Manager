decad6a333ca12543f36033932944fa4547fb4d0
dostr <d.ostrozhnoy@gmail.com>
2025-03-18 19:53:27 +0500

    Factories for tests

---
+++ TimerService/TimerService.Server.Test/Factories/TimerSessionDtoTestFactory.cs
@@ -0,0 +1,18 @@
+﻿using Manager.TimerService.Server.ServiceModels;
+
+namespace TimerService.Server.Test.Factories;
+
+public class TimerSessionDtoTestFactory: ITimerSessionDtoTestFactory
+{
+    public TimerSessionDto CreateFromTimes(DateTime startTime, DateTime? stopTime)
+    {
+        return new TimerSessionDto()
+        {
+            Id = Guid.Empty,
+            TimerId = Guid.Empty,
+            StartTime = startTime,
+            StopTime = stopTime,
+            IsOver = stopTime is not null
+        };
+    }
+}
--- TimerService/TimerService.Server.Test/TimerServiceTest.cs
+++ TimerService/TimerService.Server.Test/TimerServiceTest.cs
@@ -10,18 +10,22 @@ using TimerService.Server.Test.Factories;
 namespace TimerService.Server.Test;
 
 [TestFixture]
-public class TimerServicesTest
+public class TimerServicesTest()
 {
+    private static ITimerDtoTestFactory _timerFactory = new TimerDtoTestFactory();
+    private static ITimerSessionDtoTestFactory _sessionFactory = new TimerSessionDtoTestFactory();
+
     private ITimerRepository _timerRepository;
     private ITimerSessionService _timerSessionService;
     private IDateTimeProvider _dateTimeProvider;
     private ITimerDtoFactory _timerDtoFactory;
     private TimersService _timersService;
-    private ITimerDtoTestFactory _timerDtoTestFactory;
+
 
     [SetUp]
     public void Setup()
     {
+
         _timerRepository = Substitute.For<ITimerRepository>();
         _timerSessionService = Substitute.For<ITimerSessionService>();
         _dateTimeProvider = Substitute.For<IDateTimeProvider>();
@@ -32,7 +36,6 @@ public class TimerServicesTest
             _dateTimeProvider,
             _timerDtoFactory
         );
-        _timerDtoTestFactory = new TimerDtoTestFactory();
     }
 
     #region TestCases
@@ -42,14 +45,14 @@ public class TimerServicesTest
         yield return new TestCaseData(
             new TimerSessionDto[]
             {
-                CreateTimerSessionFromDateTimes(DateTime.Parse("2023-10-01T10:00:00"), DateTime.Parse("2023-10-01T11:00:00"))
+                _sessionFactory.CreateFromTimes(DateTime.Parse("2023-10-01T10:00:00"), DateTime.Parse("2023-10-01T11:00:00"))
             }, TimeSpan.FromHours(1));
 
         yield return new TestCaseData(
             new TimerSessionDto[]
             {
-                CreateTimerSessionFromDateTimes(DateTime.Parse("2023-10-01T10:00:00"), DateTime.Parse("2023-10-01T11:00:00")),
-                CreateTimerSessionFromDateTimes(DateTime.Parse("2023-10-01T12:00:00"), DateTime.Parse("2023-10-01T13:00:00"))
+                _sessionFactory.CreateFromTimes(DateTime.Parse("2023-10-01T10:00:00"), DateTime.Parse("2023-10-01T11:00:00")),
+                _sessionFactory.CreateFromTimes(DateTime.Parse("2023-10-01T12:00:00"), DateTime.Parse("2023-10-01T13:00:00"))
             }, TimeSpan.FromHours(2));
     }
 
@@ -58,25 +61,24 @@ public class TimerServicesTest
         yield return new TestCaseData(
             new TimerSessionDto[]
             {
-                CreateTimerSessionFromDateTimes(DateTime.Parse("2023-10-01T10:00:00"), null)
+                _sessionFactory.CreateFromTimes(DateTime.Parse("2023-10-01T10:00:00"), null)
             });
         yield return new TestCaseData(
             new TimerSessionDto[]
             {
-                CreateTimerSessionFromDateTimes(DateTime.Parse("2023-10-01T10:00:00"), DateTime.Parse("2023-10-01T11:00:00")),
-                CreateTimerSessionFromDateTimes(DateTime.Parse("2023-10-01T12:00:00"), DateTime.Parse("2023-10-01T13:00:00")),
-                CreateTimerSessionFromDateTimes(DateTime.Parse("2023-10-01T14:00:00"), null)
+                _sessionFactory.CreateFromTimes(DateTime.Parse("2023-10-01T10:00:00"), DateTime.Parse("2023-10-01T11:00:00")),
+                _sessionFactory.CreateFromTimes(DateTime.Parse("2023-10-01T12:00:00"), DateTime.Parse("2023-10-01T13:00:00")),
+                _sessionFactory.CreateFromTimes(DateTime.Parse("2023-10-01T14:00:00"), null)
             });
     }
 
     #endregion
 
-
     [Test]
     [TestCaseSource(nameof(GetCalculateElapsedTimeWithCompletedSessionsCorrectTestCases))]
     public void CalculateElapsedTimeWithCompletedSessionsCorrect(TimerSessionDto[] sessions, TimeSpan expectedElapsedTime)
     {
-        var timer = _timerDtoTestFactory.CreateFromSessions(sessions);
+        var timer = _timerFactory.CreateFromSessions(sessions);
         var result = _timersService.CalculateElapsedTime(timer);
         Assert.That(result, Is.EqualTo(expectedElapsedTime));
     }
@@ -85,26 +87,14 @@ public class TimerServicesTest
     [TestCaseSource(nameof(GetCalculateElapsedTimeWithUnCompletedSessionsCorrectTestCases))]
     public void CalculateElapsedTimeWithUnCompletedSessionsCorrect(TimerSessionDto[] sessions)
     {
-        var timer = _timerDtoTestFactory.CreateFromSessions(sessions);
+        var timer = _timerFactory.CreateFromSessions(sessions);
         Assert.Throws<ArgumentNullException>(() => _timersService.CalculateElapsedTime(timer));
     }
 
     [Test]
     public void CalculateElapsedTimeWithEmptySessionsReturnsZero()
     {
-        var timer = _timerDtoTestFactory.CreateFromSessions(new TimerSessionDto[0]);
+        var timer = _timerFactory.CreateFromSessions(new TimerSessionDto[0]);
         Assert.That(_timersService.CalculateElapsedTime(timer), Is.EqualTo(TimeSpan.Zero));
-    }
-
-    private static TimerSessionDto CreateTimerSessionFromDateTimes(DateTime startTime, DateTime? stopTime)
-    {
-        return new TimerSessionDto()
-        {
-            Id = Guid.Empty,
-            TimerId = Guid.Empty,
-            StartTime = startTime,
-            StopTime = stopTime,
-            IsOver = stopTime is not null
-        };
     }
 }
