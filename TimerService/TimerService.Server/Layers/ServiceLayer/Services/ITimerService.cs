9298039d1d5a48ab062e24d9b8d8e43f5734066c
kk7k3r <d.ostrozhnoy@gmail.com>
2025-03-26 14:04:12 +0500

    Services methods renamed

--- TimerService/TimerService.Server/Layers/ServiceLayer/Services/ITimerService.cs
+++ TimerService/TimerService.Server/Layers/ServiceLayer/Services/ITimerService.cs
@@ -6,12 +6,12 @@ namespace Manager.TimerService.Server.Layers.ServiceLayer.Services;
 
 public interface ITimerService
 {
-    Task StartTimerAsync(TimerDto timerDto);
+    Task StartAsync(TimerDto timerDto);
     Task<TimerDto[]> SelectByUserAsync(Guid userId, bool withArchived, bool withDeleted);
-    Task StopTimerAsync(Guid userId, string name, DateTime stopTime);
-    Task<TimerDto?> FindTimerAsync(Guid userId, string name);
-    Task ResetTimerAsync(Guid userId, string name);
-    Task DeleteTimerAsync(Guid userId, string name);
+    Task StopAsync(Guid userId, string name, DateTime stopTime);
+    Task<TimerDto?> FindAsync(Guid userId, string name);
+    Task ResetAsync(Guid userId, string name);
+    Task DeleteAsync(Guid userId, string name);
     TimeSpan CalculateElapsedTime(TimerDto timerDto);
-    Task ArchiveTimerAsync(TimerDto timerToArchiving);
+    Task ArchiveAsync(TimerDto timerToArchiving);
 }
