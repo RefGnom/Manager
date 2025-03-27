1e4790cd0e064cf1adf2815c16c0a2faa645cf95
kk7k3r <d.ostrozhnoy@gmail.com>
2025-03-27 17:47:49 +0500

    Refactoring. SetupHelpers added

---
+++ TimerService/TimerService.Server.Test/MockSetupHelpers/TimerFactoryMockSetupHelper.cs
@@ -0,0 +1,28 @@
+﻿using Manager.TimerService.Server.Layers.ServiceLayer.Factories;
+using Manager.TimerService.Server.ServiceModels;
+using NSubstitute;
+
+namespace TimerService.Server.Test.MockSetupHelpers;
+
+public static class TimerFactoryMockSetupHelper
+{
+    public static void ConfigureCreateArchivedTimer(
+        this ITimerDtoFactory factory,
+        TimerDto timer,
+        TimerDto archivedTimer
+    ) =>
+        factory.CreateArchived(timer)
+            .Returns(archivedTimer);
+
+    public static void ConfigureCreateResetTimer(this ITimerDtoFactory factory, TimerDto timer) =>
+        factory.CreateResetTimer(timer)
+            .Returns(timer);
+
+    public static void ConfigureCreateDeletedTimer(
+        this ITimerDtoFactory factory,
+        TimerDto timer,
+        TimerDto archivedTimer
+    ) =>
+        factory.CreateDeletedTimer(timer)
+            .Returns(archivedTimer);
+}
