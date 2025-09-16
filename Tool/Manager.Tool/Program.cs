dbd273e0ec630c221d8514b91583e8825aae05bf
Daniil <sgktost1@gmail.com>
2025-09-16 22:51:37 +0500

    Завёл serilog через кор в тулзе

--- Tool/Manager.Tool/Program.cs
+++ Tool/Manager.Tool/Program.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
-using Manager.Tool.Configuration;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Manager.Core.Common.DependencyInjection.AutoRegistration;
+using Manager.Core.Logging.Configuration;
 using Manager.Tool.Layers.Logic;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Manager.Tool;
 
@@ -9,7 +13,16 @@ public static class Program
 {
     private static async Task Main(string[] args)
     {
-        var serviceProvider = DependencyInjectionConfiguration.ConfigureServiceCollection();
+        var environment = args.Contains("-d") ? "Debug" : "Production";
+        var configuration = new ConfigurationManager();
+        configuration.AddJsonFile("appsettings.json");
+        configuration.AddJsonFile($"appsettings.{environment}.json", optional: true);
+        var serviceCollection = new ServiceCollection()
+            .AddLogging(x => x.AddConsole())
+            .UseAutoRegistrationForCurrentAssembly()
+            .UseAutoRegistrationForCoreCommon();
+        var logger = serviceCollection.AddCustomLogger(configuration, "dfgsdfgdfg");
+        var serviceProvider = serviceCollection.BuildServiceProvider();
         var managerTool = serviceProvider.GetRequiredService<IManagerTool>();
         await managerTool.RunAsync(args);
     }
