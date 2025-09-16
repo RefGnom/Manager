082da6452c27f2ee998edef4368c3d56e55403ad
Daniil <sgktost1@gmail.com>
2025-09-16 22:19:14 +0500

    Изменил метод расширения InsertBefore, чтобы если не было нужного элемента, то он вставлял вначало

--- Core/Manager.Core.Common/Linq/CommonLinqExtensions.cs
+++ Core/Manager.Core.Common/Linq/CommonLinqExtensions.cs
@@ -9,12 +9,8 @@ public static class CommonLinqExtensions
     public static void InsertBefore<TSource>(this IList<TSource> source, Func<TSource, bool> predicate, TSource element)
     {
         var beforeItem = source.FirstOrDefault(predicate);
-        if (beforeItem == null)
-        {
-            return;
-        }
 
-        var insertIndex = source.IndexOf(beforeItem);
+        var insertIndex = beforeItem != null ? source.IndexOf(beforeItem) : 0;
         source.Insert(insertIndex, element);
     }
 }
