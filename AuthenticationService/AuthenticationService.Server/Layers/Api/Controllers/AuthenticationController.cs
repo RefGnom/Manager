8c23dc363c3483bea68c7ff12e5d97dfd093289b
Daniil <sgktost1@gmail.com>
2025-09-17 17:57:30 +0500

    Создал сервис

---
+++ AuthenticationService/AuthenticationService.Server/Layers/Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Manager.AuthenticationService.Server.Layers.Api.Controllers;
+
+[ApiController]
+[Route("api/authentication")]
+public class AuthenticationController : ControllerBase
+{
+}
