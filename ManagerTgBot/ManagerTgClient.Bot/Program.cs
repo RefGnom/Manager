06916fd4dbe8c88f2d102fbac81a54760b33f834
kk7k3r <d.ostrozhnoy@gmail.com>
2025-09-19 20:07:35 +0500

    Bot created

---
+++ ManagerTgBot/ManagerTgClient.Bot/Program.cs
@@ -0,0 +1,32 @@
+﻿using ManagerTgClient.Bot;
+using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace Manager.ManagerTgClient.Bot;
+
+class Program
+{
+    public async static Task Main(string[] args)
+    {
+        using var cts = new CancellationTokenSource();
+
+
+        var configuration = new ConfigurationManager();
+        configuration.AddUserSecrets<Program>()
+            .Build();
+        var token = configuration["ManagerTgBotToken"];
+        Console.WriteLine(token);
+
+        var bot = new TelegramBotClient(configuration["ManagerTgBotToken"], cancellationToken: cts.Token);
+        var receiverOptions = new ReceiverOptions()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>()
+        };
+
+        var botHandler = new ManagerUpdateHandler();
+        bot.StartReceiving(botHandler, receiverOptions, cancellationToken: cts.Token);
+        await Task.Delay(-1);
+    }
+}
