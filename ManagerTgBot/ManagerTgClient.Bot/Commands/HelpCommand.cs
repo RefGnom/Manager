0ec5df943576e7c9d01b14b02b38a9c8034fb74f
kk7k3r <d.ostrozhnoy@gmail.com>
2025-09-20 20:52:33 +0500

    help command implemented

--- ManagerTgBot/ManagerTgClient.Bot/Commands/HelpCommand.cs
+++ ManagerTgBot/ManagerTgClient.Bot/Commands/HelpCommand.cs
@@ -1,13 +1,31 @@
-﻿using Manager.ManagerTgClient.Bot.Commands.CommandsAttributes;
+﻿using System.Reflection;
+using System.Text;
+using Manager.ManagerTgClient.Bot.Commands.CommandsAttributes;
+using Telegram.Bot;
 
 namespace Manager.ManagerTgClient.Bot.Commands;
 
 [CommandName("/help")]
 [CommandDescription("выводит подробную информацию по командам бота")]
-public class HelpCommand: IManagerBotCommand
+public class HelpCommand(
+    ITelegramBotClient botClient
+) : IManagerBotCommand
 {
-    public Task ExecuteAsync(long chatId)
+    public async Task ExecuteAsync(long chatId)
     {
-        throw new NotImplementedException();
+        StringBuilder result = new StringBuilder();
+        foreach (var command in Assembly.GetEntryAssembly()!.GetExportedTypes()
+                     .Where(x => x.GetInterface(nameof(IManagerBotCommand)) is not null
+                     ))
+        {
+            var nameAttribute = command.GetCustomAttribute<CommandNameAttribute>();
+            var descriptionAttribute = command.GetCustomAttribute<CommandDescriptionAttribute>();
+            if (nameAttribute is not null && descriptionAttribute is not null)
+            {
+                result.Append($"{nameAttribute.Value} -  {descriptionAttribute.Value}\n");
+            }
+        }
+
+        await botClient.SendMessage(chatId, result.ToString());
     }
 }
