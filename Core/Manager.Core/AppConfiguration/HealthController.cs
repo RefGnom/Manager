559756a10d014ca726c1dd12da1ee109228b41c0
Daniil <sgktost1@gmail.com>
2025-09-28 03:18:57 +0500

    базовый контроллер для проверки статуса приложения, отзывается ли

---
+++ Core/Manager.Core/AppConfiguration/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Manager.Core.AppConfiguration.Authentication;
+using Manager.Core.Common.Time;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manager.Core.AppConfiguration;
+
+[ApiController]
+[DisableAuthentication]
+[Route("[controller]")]
+public class HealthController(IDateTimeProvider dateTimeProvider) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(
+            new
+            {
+                status = "Healthy",
+                timestamp = dateTimeProvider.UtcNow,
+            }
+        );
+    }
+}
