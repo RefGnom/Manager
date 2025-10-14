ef0c75317f7e810fef909ae5072adf61fe868de1
kk7k3r <d.ostrozhnoy@gmail.com>
2025-10-14 20:14:41 +0500

    authentification service"

--- ManagerTgBot/ManagerTgClient.Bot/Repository/TelegramUserRepository.cs
+++ ManagerTgBot/ManagerTgClient.Bot/Repository/TelegramUserRepository.cs
@@ -5,6 +5,6 @@ namespace Manager.ManagerTgClient.Bot.Repository;
 
 public class TelegramUserRepository(IDataContext dataContext): ITelegramUserRepository
 {
-    public Task CreateUserAsync(User user) => dataContext.InsertAsync(user);
-    public Task<User?> FindAsync(long telegramId) => dataContext.FindAsync<User, long>(telegramId);
+    public async Task CreateUserAsync(User user) => await dataContext.InsertAsync(user);
+    public async Task<User?> FindAsync(long telegramId) => await dataContext.FindAsync<User, long>(telegramId);
 }
