beed851dcfea7f053e612fe78cb6ef8f22e78b34
Daniil <sgktost1@gmail.com>
2025-10-14 21:07:59 +0500

    Тесты на фабрики

---
+++ AuthenticationService/AuthenticationService.UnitTests/Factories/HashedAuthorizationModelFactoryTest.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Manager.AuthenticationService.Server.Layers.BusinessLogic.Factories;
+using Manager.AuthenticationService.Server.Layers.BusinessLogic.Models;
+using Manager.Core.UnitTestsCore;
+using NUnit.Framework;
+
+namespace Manager.AuthenticationService.UnitTests.Factories;
+
+public class HashedAuthorizationModelFactoryTest : UnitTestBase
+{
+    private readonly HashedAuthorizationModelFactory hashedAuthorizationModelFactory = new();
+
+    [Test]
+    public void TestCreateHashedModel()
+    {
+        // Arrange
+        var authorizationModelDto = Fixture.Create<AuthorizationModelDto>();
+        const string hash = "hash";
+
+        // Act
+        var authorizationModelWithApiKeyHashDto = hashedAuthorizationModelFactory.CreateHashedModel(
+            authorizationModelDto,
+            hash
+        );
+
+        // Assert
+        authorizationModelWithApiKeyHashDto.Should().BeEquivalentTo(authorizationModelDto);
+        authorizationModelWithApiKeyHashDto.ApiKeyHash.Should().Be(hash);
+    }
+}
