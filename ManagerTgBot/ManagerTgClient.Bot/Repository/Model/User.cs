9c8eed4b6461cebaaa7e51ae007b3f3ce380cc57
kk7k3r <d.ostrozhnoy@gmail.com>
2025-10-14 19:49:33 +0500

    user repository

--- ManagerTgBot/ManagerTgClient.Bot/Repository/Model/User.cs
+++ ManagerTgBot/ManagerTgClient.Bot/Repository/Model/User.cs
@@ -2,7 +2,7 @@
 
 public class User()
 {
-    public required Guid TelegramId { get; init; }
+    public required long TelegramId { get; init; }
     public required Guid ServerId { get; init; }
     public required string Username { get; init; }
 }
---
+++ ManagerTgBot/ManagerTgClient.Bot/Repository/TelegramUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Manager.Core.EFCore;
+using Manager.ManagerTgClient.Bot.Repository.Model;
+
+namespace Manager.ManagerTgClient.Bot.Repository;
+
+public class TelegramUserRepository(IDataContext dataContext): ITelegramUserRepository
+{
+    public Task CreateUserAsync(User user) => dataContext.InsertAsync(user);
+    public Task<User?> FindAsync(long telegramId) => dataContext.FindAsync<User, long>(telegramId);
+}
