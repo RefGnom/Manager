262a891d1f1b29000f15b188f148615eaa2c8b95
kk7k3r <d.ostrozhnoy@gmail.com>
2025-10-14 20:14:41 +0500

    authentification service"

---
+++ ManagerTgBot/ManagerTgClient.Bot/Services/AuthentificationService.cs
@@ -0,0 +1,13 @@
+﻿using Manager.ManagerTgClient.Bot.Repository;
+using Manager.ManagerTgClient.Bot.Repository.Model;
+
+namespace Manager.ManagerTgClient.Bot.Services;
+
+public class AuthentificationService(ITelegramUserRepository userRepository): IAuthentificationService
+{
+    public async Task CreateUserAsync(long telegramId, string userName) => throw new NotImplementedException();
+
+    public async Task ConnectExistingUserAsync(Guid serverId) => throw new NotImplementedException();
+
+    public async Task<User?> AuthenticateUserAsync(long telegramId) => throw new NotImplementedException();
+}
---
+++ ManagerTgBot/ManagerTgClient.Bot/Services/IAuthentificationService.cs
@@ -0,0 +1,10 @@
+﻿using Manager.ManagerTgClient.Bot.Repository.Model;
+
+namespace Manager.ManagerTgClient.Bot.Services;
+
+public interface IAuthentificationService
+{
+    Task CreateUserAsync(long telegramId, string userName);
+    Task ConnectExistingUserAsync(Guid serverId);
+    Task<User?> AuthenticateUserAsync(long telegramId);
+}
