2ebe456fe28a1a10a54c5ee66a21cefcf2fc9a74
kk7k3r <d.ostrozhnoy@gmail.com>
2025-10-14 19:21:28 +0500

    user model

--- ManagerTgBot/ManagerTgClient.Bot/Repository/Model/User.cs
+++ ManagerTgBot/ManagerTgClient.Bot/Repository/Model/User.cs
@@ -1,8 +1,3 @@
 ﻿namespace Manager.ManagerTgClient.Bot.Repository.Model;
 
-public class User()
-{
-    public required long TelegramId { get; init; }
-    public required Guid ServerId { get; init; }
-    public required string Username { get; init; }
-}
+public record User(Guid TelegramId, Guid ServerId, string UserName)
