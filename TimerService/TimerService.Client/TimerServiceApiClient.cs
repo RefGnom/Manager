db7f127049c55c6a7052c2ae9290a0a252383d46
Daniil <sgktost1@gmail.com>
2025-10-18 00:55:48 +0500

    апи клиент для сервиса таймеров

---
+++ TimerService/TimerService.Client/TimerServiceApiClient.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Manager.TimerService.Client.ServiceModels;
+
+namespace Manager.TimerService.Client;
+
+public class TimerServiceApiClient(
+    string url,
+    string apiKey
+) : ITimerServiceApiClient
+{
+    private readonly HttpClient httpClient = new()
+    {
+        BaseAddress = new Uri(url),
+        DefaultRequestHeaders = { { "X-Api-Key", apiKey } },
+    };
+
+    public async Task<HttpResponse> StartTimerAsync(StartTimerRequest startTimerRequest)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, "start")
+        {
+            Content = JsonContent.Create(startTimerRequest),
+        };
+        var responseMessage = await httpClient.SendAsync(request);
+        if (responseMessage.StatusCode == HttpStatusCode.NotFound)
+        {
+            return HttpResponse.Create(responseMessage);
+        }
+
+        responseMessage.EnsureSuccessStatusCode();
+        return HttpResponse.CreateOk();
+    }
+
+    public async Task<HttpResponse> StopTimerAsync(StopTimerRequest stopTimerRequest)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, "stop")
+        {
+            Content = JsonContent.Create(stopTimerRequest),
+        };
+        var responseMessage = await httpClient.SendAsync(request);
+        if (responseMessage.StatusCode == HttpStatusCode.NotFound)
+        {
+            return HttpResponse.Create(responseMessage);
+        }
+
+        responseMessage.EnsureSuccessStatusCode();
+        return HttpResponse.CreateOk();
+    }
+
+    public async Task<TimerResponse?> FindTimerAsync(TimerRequest timerRequest)
+    {
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"find?UserId={timerRequest.UserId}&Name={timerRequest.Name}"
+        );
+        var responseMessage = await httpClient.SendAsync(request);
+        if (responseMessage.StatusCode == HttpStatusCode.NotFound)
+        {
+            return null;
+        }
+
+        responseMessage.EnsureSuccessStatusCode();
+        return await responseMessage.Content.ReadFromJsonAsync<TimerResponse>();
+    }
+
+    public async Task<UserTimersResponse> SelectUserTimersAsync(UserTimersRequest userTimersRequest)
+    {
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"find?UserId={userTimersRequest.UserId}&" +
+            $"WithArchived={userTimersRequest.WithArchived}&" +
+            $"WithDeleted={userTimersRequest.WithDeleted}"
+        );
+        var responseMessage = await httpClient.SendAsync(request);
+        responseMessage.EnsureSuccessStatusCode();
+        return await responseMessage.Content.ReadFromJsonAsync<UserTimersResponse>() ??
+               throw new Exception("Не смогли десериализовать ответ от сервера");
+    }
+
+    public async Task<HttpResponse> ResetTimerAsync(ResetTimerRequest resetTimerRequest)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, "reset")
+        {
+            Content = JsonContent.Create(resetTimerRequest),
+        };
+        var responseMessage = await httpClient.SendAsync(request);
+        if (responseMessage.StatusCode == HttpStatusCode.NotFound)
+        {
+            return HttpResponse.Create(responseMessage);
+        }
+
+        responseMessage.EnsureSuccessStatusCode();
+        return HttpResponse.CreateOk();
+    }
+
+    public async Task<HttpResponse> DeleteTimerAsync(DeleteTimerRequest deleteTimerRequest)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Delete, "delete")
+        {
+            Content = JsonContent.Create(deleteTimerRequest),
+        };
+        var responseMessage = await httpClient.SendAsync(request);
+        if (responseMessage.StatusCode == HttpStatusCode.NotFound)
+        {
+            return HttpResponse.Create(responseMessage);
+        }
+
+        responseMessage.EnsureSuccessStatusCode();
+        return HttpResponse.CreateOk();
+    }
+}
