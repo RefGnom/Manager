9909d73e8b6d6b02092538dde30a0a2a63b5055a
Daniil <sgktost1@gmail.com>
2025-10-19 01:00:12 +0500

    guid factory

---
+++ Core/Manager.Core.Common/Factories/IGuidFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Manager.Core.Common.Factories;
+
+public interface IGuidFactory
+{
+    Guid Create();
+}
+
+public class GuidFactory : IGuidFactory
+{
+    public Guid Create() => Guid.NewGuid();
+}
