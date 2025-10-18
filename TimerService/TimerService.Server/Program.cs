b96d5b0b3753381450e0eca8789f3f43a92275ee
Daniil <sgktost1@gmail.com>
2025-10-18 18:01:32 +0500

    Убрал лишнюю конфигурацию

--- TimerService/TimerService.Server/Program.cs
+++ TimerService/TimerService.Server/Program.cs
@@ -3,13 +3,9 @@ using System.IO;
 using System.Reflection;
 using Manager.Core.AppConfiguration;
 using Manager.Core.Common.DependencyInjection.AutoRegistration;
-using Manager.Core.EFCore;
 using Manager.Core.EFCore.Configuration;
 using Manager.TimerService.Server.Configurators;
-using Manager.TimerService.Server.Layers.DbLayer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -25,33 +21,25 @@ public class Program
 
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddMapper();
-
-        builder.Services.AddDbContext<ManagerDbContext>(options =>
-            options.UseNpgsql(
-                builder.Configuration.GetSection("DataBaseOptions").Get<DataBaseOptions>()!.ConnectionString
-            )
-        );
-
         builder.Services
             .UseAutoRegistrationForCurrentAssembly()
             .UseAutoRegistrationForCoreCommon()
-            .UseNpg();
+            .UseNpg()
+            .AddSwaggerGen(c =>
+                {
+                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                    c.IncludeXmlComments(xmlPath);
+                }
+            )
+            .AddMapper();
 
         builder.Services.AddControllers();
-        builder.Services.AddSwaggerGen(c =>
-            {
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            }
-        );
 
         var app = builder.Build();
 
         app.UseRouting();
         app.MapControllers();
-
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
