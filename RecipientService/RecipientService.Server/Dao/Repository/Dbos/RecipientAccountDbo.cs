dc2127fd4d1672f93f7037be7d7fdfb5fb3afd6b
Daniil <sgktost1@gmail.com>
2025-10-18 23:13:47 +0500

    Спроектировал структуру базы для хранения аккаунта пользователя и состояний аккаунтов

---
+++ RecipientService/RecipientService.Server/Dao/Repository/Dbos/RecipientAccountDbo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Manager.RecipientService.Server.Dao.Repository.Dbos;
+
+[Table("recipient_account")]
+public class RecipientAccountDbo
+{
+    [Key, Column("id")]
+    public required Guid Id { get; set; }
+
+    [Column("login")]
+    public required string Login { get; set; }
+
+    [Column("password_hash")]
+    public required string PasswordHash { get; set; }
+
+    [Column("account_state_id")]
+    public required Guid AccountStateId { get; set; }
+
+    [Column("timezone_info")]
+    public required TimeZoneInfo TimeZoneInfo { get; set; }
+
+    [Column("created_at_utc")]
+    public required DateTime CreatedAtUtc { get; set; }
+
+    [Column("updated_at_utc")]
+    public required DateTime UpdatedAtUtc { get; set; }
+}
---
+++ RecipientService/RecipientService.Server/Implementation/Domain/StateReason.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Manager.RecipientService.Server.Implementation.Domain;
+
+public enum StateReason
+{
+    [Description("Неизвестно")]
+    Unknown,
+
+    [Description("Новый пользователь")]
+    NewUser,
+
+    [Description("Активирован администратором")]
+    ActivatedByAdmin,
+
+    [Description("Восстановлен по запросу пользователя")]
+    RestoredByUserRequest,
+
+    [Description("Разблокированный")]
+    Unbanned,
+
+    [Description("Удалён по запросу пользователя")]
+    DeletedByUserRequest,
+
+    [Description("Заблокированный")]
+    Banned,
+}
