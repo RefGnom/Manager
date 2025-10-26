cbc1b2fb2fc7488e506c48452f7fc6a929d8132e
Daniil <sgktost1@gmail.com>
2025-10-26 14:13:03 +0500

    Реализация для фабрики временных зон

--- RecipientService/RecipientService.Server/Implementation/Factories/ITimeZoneInfoFactory.cs
+++ RecipientService/RecipientService.Server/Implementation/Factories/ITimeZoneInfoFactory.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Linq;
 
 namespace Manager.RecipientService.Server.Implementation.Factories;
 
 public interface ITimeZoneInfoFactory
 {
     TimeZoneInfo CreateByOffset(TimeSpan utcOffset);
+}
+
+public class TimeZoneInfoFactory : ITimeZoneInfoFactory
+{
+    public TimeZoneInfo CreateByOffset(TimeSpan utcOffset) =>
+        TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.BaseUtcOffset == utcOffset) ??
+        throw new ArgumentException($"Не смогли определить временную зону для сдвига {utcOffset}");
 }
