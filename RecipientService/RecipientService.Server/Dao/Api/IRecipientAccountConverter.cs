b5061caef1bf4173b51293a2192644a05454ddf9
Daniil <sgktost1@gmail.com>
2025-10-26 14:13:39 +0500

    Создание аккаунта

---
+++ RecipientService/RecipientService.Server/Dao/Api/IRecipientAccountConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Manager.RecipientService.Server.Dao.Api.Requests;
+using Manager.RecipientService.Server.Dao.Api.Responses;
+using Manager.RecipientService.Server.Implementation;
+using Manager.RecipientService.Server.Implementation.Domain;
+
+namespace Manager.RecipientService.Server.Dao.Api;
+
+public interface IRecipientAccountConverter
+{
+    CreateRecipientAccountDto ToDto(CreateRecipientAccountRequest createRecipientAccountRequest);
+    RecipientAccountResponse ToResponse(RecipientAccount recipientAccount);
+}
+
+public class RecipientAccountConverter : IRecipientAccountConverter
+{
+    public CreateRecipientAccountDto ToDto(CreateRecipientAccountRequest createRecipientAccountRequest) => new(
+        createRecipientAccountRequest.Login,
+        createRecipientAccountRequest.Password,
+        TimeSpan.FromHours(createRecipientAccountRequest.RecipientTimeUtcOffsetHours)
+    );
+
+    public RecipientAccountResponse ToResponse(RecipientAccount recipientAccount) => new(
+        recipientAccount.Id,
+        recipientAccount.Login,
+        recipientAccount.State.AccountState,
+        recipientAccount.State.StateReason,
+        recipientAccount.TimeZoneInfo.BaseUtcOffset
+    );
+}
---
+++ RecipientService/RecipientService.Server/Dao/Api/RecipientAccountController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Manager.Core.AppConfiguration.Authentication;
+using Manager.RecipientService.Server.Dao.Api.Requests;
+using Manager.RecipientService.Server.Implementation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manager.RecipientService.Server.Dao.Api;
+
+[ApiController]
+[AuthorizationResource("RecipientAccount")]
+[Route("api/recipient-account")]
+public class RecipientAccountController(
+    IRecipientAccountService recipientAccountService,
+    IRecipientAccountConverter recipientAccountConverter
+) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateRecipientAccount([FromBody] CreateRecipientAccountRequest request)
+    {
+        var createRecipientAccountDto = recipientAccountConverter.ToDto(request);
+        var createResult = await recipientAccountService.CreateAsync(createRecipientAccountDto);
+        if (createResult.IsSuccess)
+        {
+            return Created(string.Empty, recipientAccountConverter.ToResponse(createResult.Value));
+        }
+
+        return createResult.Status == CreateAccountStatus.LoginAlreadyExists
+            ? Conflict(createResult.Error)
+            : BadRequest(createResult.Error);
+    }
+}
