368bfc01016f4a23f24828f48be7221dc33dfba4
Daniil <sgktost1@gmail.com>
2025-10-26 13:10:59 +0500

    Добавил сервис с одним методом - создание аккаунта

---
+++ RecipientService/RecipientService.Server/Implementation/Factories/IRecipientAccountStateFactory.cs
@@ -0,0 +1,13 @@
+﻿using Manager.RecipientService.Server.Implementation.Domain;
+
+namespace Manager.RecipientService.Server.Implementation.Factories;
+
+public interface IRecipientAccountStateFactory
+{
+    RecipientAccountState CreateInactiveByNewUser();
+}
+
+public class RecipientAccountStateFactory : IRecipientAccountStateFactory
+{
+    public RecipientAccountState CreateInactiveByNewUser() => new(AccountState.Inactive, StateReason.NewUser);
+}
