4f2c2c8dfa9910fa291a6c8d93655d428c345cef
Daniil <sgktost1@gmail.com>
2025-11-15 18:14:53 +0500

    Удалил проект core.apiclientbase, т.к. в клиента всё равно приходится тащить core.networking

--- Core/Manager.Core.Networking/HttpResult.cs
+++ Core/Manager.Core.Networking/HttpResult.cs
@@ -2,7 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Json;
 
-namespace Manager.Core.ApiClientBase;
+namespace Manager.Core.Networking;
 
 public class HttpResult(
     HttpResponseMessage httpResponseMessage
