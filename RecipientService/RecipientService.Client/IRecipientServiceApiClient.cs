b9ce863a0ba6749ef209101787dbaf905e54a2fd
Daniil <sgktost1@gmail.com>
2025-11-15 14:22:11 +0500

    Интерфейс клиента

---
+++ RecipientService/RecipientService.Client/IRecipientServiceApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Manager.Core.Common.HelperObjects.Result;
+using Manager.RecipientService.Client.BusinessObjects.Requests;
+using Manager.RecipientService.Client.BusinessObjects.Responses;
+
+namespace Manager.RecipientService.Client;
+
+public interface IRecipientServiceApiClient
+{
+    Task<ProcessResult<string, HttpStatusCode>> CreateRecipientAccountAsync(CreateRecipientAccountRequest request);
+    Task<RecipientAccountResponse?> FindRecipientAccountAsync(Guid recipientId);
+    Task<ProcessResult<string, HttpStatusCode>> DeleteRecipientAccountAsync(Guid recipientId);
+    Task<ProcessResult<string, HttpStatusCode>> UpdateRecipientAccountAsync(PatchRecipientAccountRequest request);
+    Task<RecipientAuthorizationResponse?> FindRecipientAuthorizationAsync(RecipientAuthorizationRequest request);
+}
