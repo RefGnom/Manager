971abdefb991f4219013b4cd14a603149b46433e
Daniil <sgktost1@gmail.com>
2025-11-15 15:18:43 +0500

    Фабрика апи клиента

---
+++ RecipientService/RecipientService.Client/IRecipientServiceApiClientFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Manager.RecipientService.Client;
+
+public interface IRecipientServiceApiClientFactory
+{
+    IRecipientServiceApiClient Create(string port, string apiKey);
+}
+
+public class RecipientServiceApiClientFactory : IRecipientServiceApiClientFactory
+{
+    public IRecipientServiceApiClient Create(string port, string apiKey) =>
+        new RecipientServiceApiClient($"http://localhost:{port}/api", apiKey);
+}
