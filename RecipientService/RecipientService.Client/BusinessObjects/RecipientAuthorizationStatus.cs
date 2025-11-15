f60cc8ad5b378454045791c95ed41863798b6515
Daniil <sgktost1@gmail.com>
2025-11-15 14:21:02 +0500

    BusinessObjects. Хочется уже доменку отдельным проектом делать

---
+++ RecipientService/RecipientService.Client/BusinessObjects/RecipientAuthorizationStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Manager.RecipientService.Client.BusinessObjects;
+
+public enum RecipientAuthorizationStatus
+{
+    [Description("Доступ разрешён")]
+    Success = 0,
+
+    [Description("Доступ запрещён, т.к. аккаунт не в активном состоянии")]
+    AccountIsNotActive = 1,
+
+    [Description("Доступ к сервису запрещён")]
+    AccessToServiceDenied = 2,
+
+    [Description("Доступ к ресурсу запрещён")]
+    AccessToResourceDenied = 3,
+}
---
+++ RecipientService/RecipientService.Client/BusinessObjects/Responses/RecipientAuthorizationResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Manager.RecipientService.Client.BusinessObjects.Responses;
+
+public record RecipientAuthorizationResponse(
+    Guid RecipientId,
+    string RequestedService,
+    string RequestedResource,
+    RecipientAuthorizationStatus RecipientAuthorizationStatus
+);
