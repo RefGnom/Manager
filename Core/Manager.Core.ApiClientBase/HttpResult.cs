27e11bf61cd6fb85f4a1244382af2150cda6074d
Daniil <sgktost1@gmail.com>
2025-11-15 14:59:41 +0500

    Поменял ProcessResult на новый HttpResult

---
+++ Core/Manager.Core.ApiClientBase/HttpResult.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace Manager.Core.ApiClientBase;
+
+public class HttpResult(
+    HttpResponseMessage httpResponseMessage
+)
+{
+    public HttpStatusCode StatusCode => httpResponseMessage.StatusCode;
+    public bool IsSuccess => httpResponseMessage.IsSuccessStatusCode;
+    public string ResultMessage => httpResponseMessage.ReasonPhrase ?? string.Empty;
+
+    public static implicit operator HttpResult(HttpResponseMessage httpResponse) => new(httpResponse);
+}
+
+public class HttpResult<TResponse>(
+    HttpResponseMessage httpResponseMessage,
+    TResponse? response
+) : HttpResult(httpResponseMessage)
+{
+    private readonly HttpResponseMessage httpResponseMessage = httpResponseMessage;
+
+    public bool IsNotFound => httpResponseMessage.StatusCode == HttpStatusCode.NotFound;
+
+    public TResponse EnsureResponse
+    {
+        get
+        {
+            httpResponseMessage.EnsureSuccessStatusCode();
+            return response ?? throw new HttpRequestException();
+        }
+    }
+
+    public static implicit operator HttpResult<TResponse>(HttpResponseMessage httpResponse) => new(
+        httpResponse,
+        httpResponse.Content.ReadFromJsonAsync<TResponse>().GetAwaiter().GetResult()
+    );
+}
