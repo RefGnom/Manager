d13fbc06deb92896bcf6b3f4c0bbdd2ee14caf4c
Alex-Vay <vatlin04@mail.ru>
2025-11-18 01:41:17 +0500

    other fix

--- Core/Manager.Core.Networking.IntegrationTests/SetupFixture.cs
+++ Core/Manager.Core.Networking.IntegrationTests/SetupFixture.cs
@@ -1,12 +1,13 @@
 using System.Reflection;
 using Manager.Core.IntegrationTestsCore;
 using Manager.Core.IntegrationTestsCore.Configuration;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace Manager.Core.Networking.IntegrationTests;
 
-public class NetworkingSetupFixture : SetupFixtureBase
+public class SetupFixture : SetupFixtureBase
 {
-    protected override Assembly TargetTestingAssembly => typeof(NetworkingSetupFixture).Assembly;
+    protected override Assembly TargetTestingAssembly => typeof(Program).Assembly;
 
     protected override void CustomizeConfigurationBuilder(IIntegrationTestConfigurationBuilder builder)
     {
