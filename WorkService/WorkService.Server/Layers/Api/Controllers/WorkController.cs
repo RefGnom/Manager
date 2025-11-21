2114fbbfb718c3fbb08496db88b8fcf18b12e676
kk7k3r <d.ostrozhnoy@gmail.com>
2025-11-21 17:53:14 +0500

    update work api to REST

--- WorkService/WorkService.Server/Layers/Api/Controllers/WorkController.cs
+++ WorkService/WorkService.Server/Layers/Api/Controllers/WorkController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Manager.Core.AppConfiguration.Authentication;
 using Manager.WorkService.Server.Layers.Api.Converters;
@@ -12,14 +13,14 @@ namespace Manager.WorkService.Server.Layers.Api.Controllers;
 
 [ApiController]
 [AuthorizationResource("WorksCrud")]
-[Route("api/works")]
+[Route("api/recipients/{recipientId:guid}/works")]
 public class WorkController(
     IWorkService workService,
     IWorkApiConverter workApiConverter
 ) : ControllerBase
 {
     [HttpPost]
-    public async Task<ActionResult<Guid>> CreateWork([FromBody] CreateWorkRequest request)
+    public async Task<ActionResult<Guid>> CreateWork(CreateWorkRequest request)
     {
         var workId = Guid.NewGuid();
         var workDto = workApiConverter.ToDto(request, workId);
@@ -41,7 +42,7 @@ public class WorkController(
     }
 
     [HttpPatch]
-    public async Task<IActionResult> PatchWork([FromBody] PatchWorkRequest request)
+    public async Task<IActionResult> PatchWork(PatchWorkRequest request)
     {
         var workDto = await workService.FindWorkAsync(request.Id);
         if (workDto == null)
@@ -65,5 +66,47 @@ public class WorkController(
 
         await workService.DeleteWorkAsync(workDto);
         return Ok();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<GetWorkResponse[]>> GetWorks([FromRoute] Guid recipientId)
+    {
+        var recipientWorks = await workService.SelectWorksAsync(recipientId);
+        return Ok(recipientWorks.Select(workApiConverter.ToResponse));
+    }
+
+    [HttpGet("actual")]
+    public async Task<ActionResult<GetWorkResponse[]>> GetActualWorks([FromRoute] Guid recipientId)
+    {
+        var recipientWorks = await workService.SelectActualWorksAsync(recipientId);
+        return Ok(recipientWorks.Select(workApiConverter.ToResponse));
+    }
+
+    [HttpGet("ready-for-reminder")]
+    public async Task<ActionResult<GetWorkResponse[]>> GetWorksForReminder([FromRoute] Guid recipientId)
+    {
+        var recipientWorks = await workService.SelectWorksForReminderAsync(recipientId);
+        return Ok(recipientWorks.Select(workApiConverter.ToResponse));
+    }
+
+    [HttpGet("expired")]
+    public async Task<ActionResult<GetWorkResponse[]>> GetExpiredWorks([FromRoute] Guid recipientId)
+    {
+        var recipientWorks = await workService.SelectExpiredWorksAsync(recipientId);
+        return Ok(recipientWorks.Select(workApiConverter.ToResponse));
+    }
+
+    [HttpGet("deleted")]
+    public async Task<ActionResult<GetWorkResponse[]>> GetDeletedWorks([FromRoute] Guid recipientId)
+    {
+        var recipientWorks = await workService.SelectDeletedWorksAsync(recipientId);
+        return Ok(recipientWorks.Select(workApiConverter.ToResponse));
+    }
+
+    [HttpGet("completed")]
+    public async Task<ActionResult<GetWorkResponse[]>> GetCompletedWorks([FromRoute] Guid recipientId)
+    {
+        var recipientWorks = await workService.SelectCompletedWorksAsync(recipientId);
+        return Ok(recipientWorks.Select(workApiConverter.ToResponse));
     }
 }
