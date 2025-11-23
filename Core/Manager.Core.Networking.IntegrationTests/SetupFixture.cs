8d48a17f8c372afa1f87ebc4cd480e219640f411
Daniil <sgktost1@gmail.com>
2025-11-23 15:17:30 +0500

    Правлю конфигурацию инт тестов Core.Networking

--- Core/Manager.Core.Networking.IntegrationTests/SetupFixture.cs
+++ Core/Manager.Core.Networking.IntegrationTests/SetupFixture.cs
@@ -1,16 +1,15 @@
 using System.Reflection;
 using Manager.Core.IntegrationTestsCore;
 using Manager.Core.IntegrationTestsCore.Configuration;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace Manager.Core.Networking.IntegrationTests;
 
 public class SetupFixture : SetupFixtureBase
 {
-    protected override Assembly TargetTestingAssembly => typeof(Program).Assembly;
+    protected override Assembly TargetTestingAssembly => typeof(IPortProvider).Assembly;
 
     protected override void CustomizeConfigurationBuilder(IIntegrationTestConfigurationBuilder builder)
     {
-        builder.WithRealLogger();
+        builder.WithRealLogger().WithoutDataBase().WithoutAutoRegistration();
     }
 }
