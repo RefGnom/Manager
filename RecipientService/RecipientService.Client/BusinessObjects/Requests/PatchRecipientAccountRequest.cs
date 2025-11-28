9f16e3c00031c2555057b1ef14fe3a530c16ded2
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2025-11-28 20:47:58 +0500

    :(

--- RecipientService/RecipientService.Client/BusinessObjects/Requests/PatchRecipientAccountRequest.cs
+++ RecipientService/RecipientService.Client/BusinessObjects/Requests/PatchRecipientAccountRequest.cs
@@ -6,7 +6,7 @@ namespace Manager.RecipientService.Client.BusinessObjects.Requests;
 public class PatchRecipientAccountRequest
 {
     [JsonIgnore]
-    public required Guid RecipientId { get; init; }
+    public Guid RecipientId { get; init; }
     public string? NewLogin { get; init; }
     public string? NewPassword { get; init; }
     public int? NewRecipientTimeUtcOffsetHours { get; init; }
