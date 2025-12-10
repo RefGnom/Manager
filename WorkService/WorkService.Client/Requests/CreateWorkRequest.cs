7b129819deb623ec06adb518757f4c3f5e51802e
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2025-12-10 15:33:35 +0500

    fix

--- WorkService/WorkService.Client/Requests/CreateWorkRequest.cs
+++ WorkService/WorkService.Client/Requests/CreateWorkRequest.cs
@@ -5,7 +5,7 @@ namespace Manager.WorkService.Client.Requests;
 public record CreateWorkRequest(
     Guid RecipientId,
     string Title,
-    string? Description,
-    DateTime? DeadLineUtc,
+    string? Description = null,
+    DateTime? DeadLineUtc = null,
     TimeSpan[]? ReminderIntervals = null
 );
