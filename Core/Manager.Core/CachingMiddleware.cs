42ce8aaf04ba323cf4253d76179a533c1722db04
fireilya <fireilya3000@gmail.com>
2025-12-21 15:39:29 +0500

    Bagofix 1

--- Core/Manager.Core/CachingMiddleware.cs
+++ Core/Manager.Core/CachingMiddleware.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
@@ -10,7 +10,8 @@ namespace Manager.Core;
 public class CachingMiddleware(
     RequestDelegate next,
     ILogger logger,
-    IDistributedCache cache)
+    IDistributedCache cache
+)
 {
     private readonly DistributedCacheEntryOptions distributedCacheEntryOptions = new()
     {
@@ -19,32 +20,74 @@ public class CachingMiddleware(
 
     public async Task InvokeAsync(HttpContext context)
     {
-        if (context.Request.Method == "GET")
+        if (!IsCacheNeeded(context))
         {
-            logger.LogDebug("Cache invoked");
-            var cacheResponse = await cache.GetAsync(context.Request.Path);
-            if (cacheResponse is not null)
-            {
-                logger.LogDebug("Cache hit for {RequestPath}", context.Request.Path);
-                context.Response.StatusCode = 200;
-                await context.Response.Body.WriteAsync(cacheResponse);
-                return;
-            }
-            logger.LogDebug("Cache miss for {RequestPath}", context.Request.Path);
+            await next(context);
+            return;
+        }
+
+        logger.LogInformation("Cache invoked");
+        var cacheKey = $"{context.Request.Path.ToString()}?{context.Request.QueryString}";
+        if (!TryGetCacheByKey(cacheKey, out var cacheResponse))
+        {
             await next.Invoke(context);
-            if (context.Response.StatusCode == 200)
-            {
-                var byteBuffer = new byte[context.Request.Body.Length];
-                await context.Response.Body.ReadExactlyAsync(byteBuffer, 0, byteBuffer.Length);
-                await cache.SetAsync(context.Request.Path, byteBuffer, distributedCacheEntryOptions);
-                logger.LogDebug("New cache fot {RequestPath} registered", context.Request.Path);
-            }
+            return;
+        }
+
+        if (cacheResponse is not null)
+        {
+            logger.LogDebug("Cache hit for {RequestPath}", cacheKey);
+            context.Response.StatusCode = 200;
+            await context.Response.Body.WriteAsync(cacheResponse);
+            return;
+        }
+
+        logger.LogInformation("Cache miss for {RequestPath}", cacheKey);
+        var originalBody = context.Response.Body;
+        var bodyDummy = new MemoryStream();
+        context.Response.Body = bodyDummy;
+        await next.Invoke(context);
+        if (context.Response.StatusCode is >= 200 and < 300)
+        {
+            var byteBuffer = new byte[bodyDummy.Length];
+            await bodyDummy.ReadExactlyAsync(byteBuffer, 0, byteBuffer.Length);
+            bodyDummy.Seek(0, SeekOrigin.Begin);
+            await bodyDummy.CopyToAsync(originalBody);
+            context.Response.Body = originalBody;
+            TrySetCacheByKey(cacheKey, byteBuffer);
+            logger.LogInformation("New cache fot {RequestPath} registered", context.Request.Path);
         }
     }
-}
 
-public static class CachingMiddlewareExtensions
-{
-    public static IApplicationBuilder UseCachingMiddleware(this IApplicationBuilder app) =>
-        app.UseMiddleware<CachingMiddleware>();
+    private bool IsCacheNeeded(HttpContext context)
+    {
+        return context.Request.Method == "GET";
+    }
+
+    private bool TryGetCacheByKey(string cacheKey, out byte[]? response)
+    {
+        response = null;
+        try
+        {
+            response = cache.Get(cacheKey);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Can't get cache value because cache is unavailable");
+            return false;
+        }
+    }
+
+    private async Task TrySetCacheByKey(string cacheKey, byte[] data)
+    {
+        try
+        {
+            await cache.SetAsync(cacheKey, data, distributedCacheEntryOptions);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Can't set cache value because cache is unavailable");
+        }
+    }
 }
