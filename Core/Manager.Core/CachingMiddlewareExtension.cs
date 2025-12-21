a21acfa9daa2c1c999fb3e9af7875439d7344a75
fireilya <fireilya3000@gmail.com>
2025-12-21 15:39:29 +0500

    Bagofix 1

---
+++ Core/Manager.Core/CachingMiddlewareExtension.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Manager.Core;
+
+public static class CachingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCachingMiddleware(this IApplicationBuilder app) =>
+        app.UseMiddleware<CachingMiddleware>();
+}
