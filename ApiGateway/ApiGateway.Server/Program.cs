5cb36d6e3c9791832e50062f1c1e52babf2e8437
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2025-12-22 17:19:34 +0500

    f

--- ApiGateway/ApiGateway.Server/Program.cs
+++ ApiGateway/ApiGateway.Server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +12,11 @@ builder.Services
 
 var app = builder.Build();
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 
 app.MapReverseProxy();
