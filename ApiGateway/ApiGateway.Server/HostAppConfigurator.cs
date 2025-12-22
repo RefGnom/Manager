3cd8883aa7f2e2c2a4e72b1efecfab0fc45e565a
fireilya <fireilya3000@gmail.com>
2025-12-22 19:55:49 +0500

    Caching added for api gateway

---
+++ ApiGateway/ApiGateway.Server/HostAppConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using Manager.Core.HostApp;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Manager.ApiGateway.Server;
+
+#pragma warning disable CS1591
+public class HostAppConfigurator : IHostAppConfigurator
+{
+    public void ConfigureServiceCollection(IServiceCollection serviceCollection, ILogger logger) { }
+
+    public void ConfigureApplication(IApplicationBuilder applicationBuilder, ILogger logger) { }
+
+    public void ConfigureSwaggerOption(SwaggerGenOptions options) { }
+}
