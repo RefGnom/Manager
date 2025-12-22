10316d62e6667c3f048eb100b4f836b513016dc7
fireilya <fireilya3000@gmail.com>
2025-12-23 01:18:35 +0500

    please work *crying*

--- ApiGateway/ApiGateway.IntegrationalTests/CachingIntegrationalTests.cs
+++ ApiGateway/ApiGateway.IntegrationalTests/CachingIntegrationalTests.cs
@@ -1,10 +1,12 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Manager.Core.IntegrationTestsCore;
 using Manager.Core.Networking;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 
@@ -16,33 +18,16 @@ public class TimerClientTest : IntegrationTestBase
     public async Task TestApiGatewayCache()
     {
         // Arrange
-        var client = new HttpClient();
-        var portProvider = ServiceProvider.GetRequiredService<IPortProvider>();
-
+        var cache = ServiceProvider.GetRequiredService<IDistributedCache>();
+        const string cacheKey = "testKey";
+        var cacheValue = "testValue".Select(x => (byte)x).ToArray();
         // Act
-        var healthRequest = new HttpRequestMessage(
-            HttpMethod.Get,
-            $"http://localhost:{portProvider.GetPort("API_GATEWAY_PORT")}/Health"
-        );
-        var httpResponse = await client.SendAsync(healthRequest);
+        cache.Should().NotBeNull("Низя");
+        await cache.SetAsync(cacheKey, cacheValue);
 
         // Asset
-
-        // Asset
-        httpResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-        var baseReturnedAnswer = await httpResponse.Content.ReadAsStringAsync();
-        Thread.Sleep((int)(2 * 1e3));
-
-        httpResponse = await client.SendAsync(await healthRequest.CloneAsync());
-        httpResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-        var newReturnedAnswer = await httpResponse.Content.ReadAsStringAsync();
-        newReturnedAnswer.Should().BeEquivalentTo(baseReturnedAnswer);
-        Thread.Sleep((int)(40 * 1e3));
-
-        httpResponse = await client.SendAsync(await healthRequest.CloneAsync());
-        httpResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-        newReturnedAnswer = await httpResponse.Content.ReadAsStringAsync();
-        newReturnedAnswer.Should().NotBeEquivalentTo(baseReturnedAnswer);
+        var cacheResponse = await cache.GetAsync(cacheKey);
+        cacheResponse.Should().BeEquivalentTo(cacheValue);
 
         /*
          * Господь не любит программистов
