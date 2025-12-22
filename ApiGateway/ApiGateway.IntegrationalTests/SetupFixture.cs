ece7cf1c7ba13397271cab05f2e52082ce81c9ce
fireilya <fireilya3000@gmail.com>
2025-12-22 20:50:44 +0500

    Cache middleware fixed

---
+++ ApiGateway/ApiGateway.IntegrationalTests/SetupFixture.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Manager.ApiGateway.Server;
+using Manager.Core.IntegrationTestsCore;
+using Manager.Core.IntegrationTestsCore.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ApiGateway.IntegrationalTests;
+
+public class ApiGatewaySetupFixture : SetupFixtureBase
+{
+    protected override Assembly TargetTestingAssembly => typeof(Program).Assembly;
+
+    protected override void CustomizeConfigurationBuilder(IIntegrationTestConfigurationBuilder builder)
+    {
+        builder.WithLocalServer(
+            new Dictionary<string, string>
+            {
+                ["AuthenticationServiceSetting:ApiKey"] = "random key",
+            }
+        ).WithRealLogger();
+    }
+
+    protected override void CustomizeServiceCollection(IServiceCollection serviceCollection)
+    {
+    }
+}
