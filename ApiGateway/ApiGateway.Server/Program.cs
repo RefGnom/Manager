6c5325a3a5496abe1712d0bbe493b1de6390ef78
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2025-12-22 17:03:07 +0500

    Api.Gateway

--- ApiGateway/ApiGateway.Server/Program.cs
+++ ApiGateway/ApiGateway.Server/Program.cs
@@ -1,18 +1,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.MapReverseProxy();
 app.Run();
