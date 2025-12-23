673bceb34c6804be4f0ba27095a0f289031417b4
fireilya <fireilya3000@gmail.com>
2025-12-23 23:22:38 +0500

    http client autoregistation fix with retry

--- Core/Manager.Core.Common/Reflection/TypeExtensions.cs
+++ Core/Manager.Core.Common/Reflection/TypeExtensions.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 
 namespace Manager.Core.Common.Reflection;
 
 public static class TypeExtensions
 {
     public static bool HasInterface<TInterface>(this Type type) => type.GetInterfaces().Contains(typeof(TInterface));
+
+    public static bool HasAttribute<TAttribute>(this Type type) where TAttribute : Attribute =>
+        type.GetCustomAttribute<TAttribute>() is not null;
 }
