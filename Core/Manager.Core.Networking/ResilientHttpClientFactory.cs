cd37b106ee4a76a7a7db3357f80ac0bd3d8111e2
Alex-Vay <vatlin04@mail.ru>
2025-12-25 00:49:28 +0500

    test add fallback

--- Core/Manager.Core.Networking/ResilientHttpClientFactory.cs
+++ Core/Manager.Core.Networking/ResilientHttpClientFactory.cs
@@ -1,22 +1,36 @@
 using System;
 using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace Manager.Core.Networking;
 
-public interface IResilientHttpClientFactory
-{
-    IHttpClient CreateClient(string url, string apiKey);
-}
-
-public class ResilientHttpClientFactory : IResilientHttpClientFactory
+public class ResilientHttpClientFactory(
+    IConfiguration configuration
+) : IResilientHttpClientFactory
 {
     public IHttpClient CreateClient(string url, string apiKey)
     {
+        var section = configuration.GetSection("HttpClientOptions");
+
+        var enableFallback = section.GetValue<bool>("EnableFallback"); // false
+        var retryCount = section.GetValue<int?>("RetryCount") ?? 3;
+
+        var retryDelayMs = section.GetValue<int?>("RetryDelayMs");
+        TimeSpan? fixedDelay = retryDelayMs.HasValue ? TimeSpan.FromMilliseconds(retryDelayMs.Value) : null;
+
+        var timeoutMs = section.GetValue<int?>("TimeoutMs");
+
         var httpClient = new HttpClient
         {
             BaseAddress = new Uri(url),
             DefaultRequestHeaders = { { "X-Api-Key", apiKey } },
         };
-        return new ResilientHttpClient(httpClient);
+
+        if (timeoutMs.HasValue)
+        {
+            httpClient.Timeout = TimeSpan.FromMilliseconds(timeoutMs.Value);
+        }
+
+        return new ResilientHttpClient(httpClient, enableFallback, retryCount, fixedDelay);
     }
 }
