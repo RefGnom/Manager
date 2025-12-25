2ac63dcfd482187edca3091008889701da145768
Daniil <sgktost1@gmail.com>
2025-12-26 00:08:17 +0500

    трансформер запросов к сервисам, который добавляем апи ключи в заголовки

---
+++ ApiGateway/ApiGateway.Server/Configuration/ApiKeyTransformProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using Yarp.ReverseProxy.Transforms;
+using Yarp.ReverseProxy.Transforms.Builder;
+
+namespace Manager.ApiGateway.Server.Configuration;
+
+public class ApiKeyTransformProvider(
+    IOptions<ApiKeysOptions> options
+) : ITransformProvider
+{
+    private readonly ApiKeysOptions apiKeysOptions = options.Value;
+
+    public void ValidateRoute(TransformRouteValidationContext context) { }
+    public void ValidateCluster(TransformClusterValidationContext context) { }
+
+    public void Apply(TransformBuilderContext context)
+    {
+        if (context.Cluster == null)
+        {
+            return;
+        }
+
+        var clusterId = context.Cluster.ClusterId;
+        var apiKey = apiKeysOptions.GetApiKey(clusterId);
+        context.AddRequestHeader("X-Api-Key", apiKey);
+    }
+}
