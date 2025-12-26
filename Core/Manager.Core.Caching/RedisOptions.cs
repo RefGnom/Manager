88f8fe85f761ff2526cbc46e8ede61b3f208af26
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2025-12-26 17:19:11 +0500

    reddis port default value

--- Core/Manager.Core.Caching/RedisOptions.cs
+++ Core/Manager.Core.Caching/RedisOptions.cs
@@ -3,7 +3,7 @@
 public class RedisOptions
 {
     public string? Host { get; init; }
-    public int Port { get; init; }
+    public int Port { get; init; } = 6379;
     public string? Password { get; init; }
     public string? TimeoutInMs { get; init; }
 }
